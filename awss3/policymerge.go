dc1960444a37eea26eb660f9d03753dd358317e5
Rachel Aurand <rachel@countableset.com>
2019-01-06 14:30:16 -0800

    Refactor policy traversal

--- awss3/policymerge.go
+++ awss3/policymerge.go
@@ -2,25 +2,85 @@ package awss3
 
 import (
 	"sort"
+
+	"gitlab.com/countableset/lambda-s3-cloudflare/util"
 )
 
 // GetSortedIPAddresses returns the list of ip address sorts from the policy
 func GetSortedIPAddresses(policy map[string]interface{}) []string {
-	firstStatement := policy["Statement"].([]interface{})[0].(map[string]interface{})
-	condition := firstStatement["Condition"].(map[string]interface{})
-	ipAddr := condition["IpAddress"].(map[string]interface{})["aws:SourceIp"].([]interface{})
-	ipAddresses := make([]string, len(ipAddr))
-	for i, v := range ipAddr {
+	_, c := findBlock(policy)
+	if c == nil {
+		util.ExitErrorf("Unable to find ip address field in policy %v", policy)
+	}
+	addressesInterface := (*c).([]interface{})
+	ipAddresses := make([]string, len(addressesInterface))
+	for i, v := range addressesInterface {
 		ipAddresses[i] = v.(string)
 	}
 	sort.Strings(ipAddresses)
 	return ipAddresses
 }
 
-// MergeIPSliceIntoPolicy todo
+// MergeIPSliceIntoPolicy returns an updated policy with the merges ip addresses for the parameter
 func MergeIPSliceIntoPolicy(policy map[string]interface{}, ipAddresses []string) interface{} {
-	firstStatement := policy["Statement"].([]interface{})[0].(map[string]interface{})
-	condition := firstStatement["Condition"].(map[string]interface{})
-	condition["IpAddress"].(map[string]interface{})["aws:SourceIp"] = ipAddresses
+	p, c := findBlock(policy)
+	if c == nil {
+		util.ExitErrorf("Unable to find ip address field in policy %v", policy)
+	}
+	(*p).(map[string]interface{})["aws:SourceIp"] = ipAddresses
 	return policy
+}
+
+func findBlock(root interface{}) (*interface{}, *interface{}) {
+	conditions := []func(map[string]interface{}) (interface{}, bool){
+		func(field map[string]interface{}) (interface{}, bool) {
+			item, ok := field["Statement"]
+			return item, ok
+		},
+		func(field map[string]interface{}) (interface{}, bool) {
+			if action, ok := field["Action"]; ok && action == "s3:GetObject" {
+				item, ok := field["Condition"]
+				return item, ok
+			}
+			return nil, false
+		},
+		func(field map[string]interface{}) (interface{}, bool) {
+			item, ok := field["IpAddress"]
+			return item, ok
+		},
+		func(field map[string]interface{}) (interface{}, bool) {
+			item, ok := field["aws:SourceIp"]
+			return item, ok
+		},
+	}
+	return testBlock(nil, &root, 0, &conditions)
+}
+
+func testBlock(
+	parent *interface{},
+	branch *interface{},
+	index int,
+	conditions *[]func(map[string]interface{}) (interface{}, bool),
+) (*interface{}, *interface{}) {
+	if index < len(*conditions) {
+		switch branchType := (*branch).(type) {
+		case []interface{}:
+			for _, item := range branchType {
+				p, result := testBlock(branch, &item, index, conditions)
+				if result != nil {
+					return p, result
+				}
+			}
+		case map[string]interface{}:
+			if field, ok := (*conditions)[index](branchType); ok {
+				return testBlock(branch, &field, index+1, conditions)
+			} else {
+				return nil, nil
+			}
+		default:
+			util.ExitErrorf("Invalid type of branch=%v", branch)
+		}
+		return nil, nil
+	}
+	return parent, branch
 }
--- awss3/policymerge_test.go
+++ awss3/policymerge_test.go
@@ -2,40 +2,58 @@ package awss3
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func TestGetSortedIPAddresses(t *testing.T) {
-	var tests = []struct {
-		input    string
-		expected []string
-	}{
-		{"{\"Statement\":[{\"Condition\":{\"IpAddress\":{\"aws:SourceIp\":[\"2400:cb00::/32\",\"2405:8100::/32\",\"162.158.0.0/15\"]}}}]}",
-			[]string{"162.158.0.0/15", "2400:cb00::/32", "2405:8100::/32"},
-		},
-	}
-	for _, test := range tests {
-		resp := s3.GetBucketPolicyOutput{Policy: &test.input}
-		result := parsePolicyResponse(&resp)
-		resultMap := result.(map[string]interface{})
-		addresses := GetSortedIPAddresses(resultMap)
-		if len(addresses) != len(test.expected) {
-			t.Errorf("Expected %d but got %d", len(test.expected), len(addresses))
-		}
-		for i, v := range test.expected {
-			if addresses[i] != v {
-				t.Errorf("Expected %s but got %s", v, addresses[i])
+	input := `{
+		"Statement": [
+		  {
+			"Action": "s3:GetObject",
+			"Condition": {
+			  "IpAddress": {
+				"aws:SourceIp": ["2400:cb00::/32", "2405:8100::/32", "162.158.0.0/15"]
+			  }
 			}
+		  }
+		]
+	  }
+	  `
+	expected := []string{"162.158.0.0/15", "2400:cb00::/32", "2405:8100::/32"}
+
+	resp := s3.GetBucketPolicyOutput{Policy: &input}
+	result := parsePolicyResponse(&resp)
+	resultMap := result.(map[string]interface{})
+	addresses := GetSortedIPAddresses(resultMap)
+	if len(addresses) != len(expected) {
+		t.Errorf("Expected %d but got %d", len(expected), len(addresses))
+	}
+	for i, v := range expected {
+		if addresses[i] != v {
+			t.Errorf("Expected %s but got %s", v, addresses[i])
 		}
 	}
 }
 
 func TestMergeIPSliceIntoPolicy(t *testing.T) {
-	expected := "{\"Statement\":[{\"Condition\":{\"IpAddress\":{\"aws:SourceIp\":[\"a\",\"b\"]}}}]}"
+	expected := "{\"Statement\":[{\"Action\":\"s3:GetObject\",\"Condition\":{\"IpAddress\":{\"aws:SourceIp\":[\"a\",\"b\"]}}}]}"
 
-	policyStr := "{\"Statement\":[{\"Condition\":{\"IpAddress\":{\"aws:SourceIp\":[\"2400:cb00::/32\",\"2405:8100::/32\",\"162.158.0.0/15\"]}}}]}"
+	policyStr := `{
+		"Statement": [
+		  {
+			"Action": "s3:GetObject",
+			"Condition": {
+			  "IpAddress": {
+				"aws:SourceIp": ["2400:cb00::/32", "2405:8100::/32", "162.158.0.0/15"]
+			  }
+			}
+		  }
+		]
+	  }
+	  `
 	resp := s3.GetBucketPolicyOutput{Policy: &policyStr}
 	policy := parsePolicyResponse(&resp)
 	policyMap := policy.(map[string]interface{})
@@ -47,5 +65,228 @@ func TestMergeIPSliceIntoPolicy(t *testing.T) {
 	}
 	if string(encoded) != expected {
 		t.Errorf("Expected %s but got %s", expected, string(encoded))
+	}
+}
+
+func TestFindBlock(t *testing.T) {
+	tests := []struct {
+		input string
+	}{
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:SourceIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  }
+			]
+		  }`},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "VisualEditor",
+				"Effect": "Allow",
+				"Action": "rout53:Edit",
+				"Resource": "arn:aws:*"
+			  },
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:SourceIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  }
+			]
+		  }`},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "Forbidden": true
+				}
+			  },
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:SourceIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  }
+			]
+		  }
+		  `},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:TargetIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  },
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:SourceIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  }
+			]
+		  }
+		  `},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:SourceIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  },
+			  {
+				"Sid": "VisualEditor",
+				"Effect": "Allow",
+				"Action": "rout53:Edit",
+				"Resource": "arn:aws:*"
+			  }
+			]
+		  }
+		  `},
+	}
+	for _, test := range tests {
+		resp := s3.GetBucketPolicyOutput{Policy: &test.input}
+		policy := parsePolicyResponse(&resp)
+
+		_, c := findBlock(policy)
+		if reflect.TypeOf(*c).Kind() != reflect.Slice {
+			t.Errorf("Expected type of slice")
+		}
+		addresses := (*c).([]interface{})
+		if len(addresses) != 2 {
+			t.Errorf("Expected %d but got %d", 2, len(addresses))
+		}
+		if addresses[0].(string) != "2400:cb00::/32" {
+			t.Errorf("Expected %s but got %s", "2400:cb00::/32", addresses[0].(string))
+		}
+		if addresses[1].(string) != "162.158.0.0/15" {
+			t.Errorf("Expected %s but got %s", "162.158.0.0/15", addresses[1].(string))
+		}
+	}
+}
+
+func TestFindNothingInBlock(t *testing.T) {
+	tests := []struct {
+		input string
+	}{
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "IpAddress": {
+					"aws:TargetIp": ["2400:cb00::/32", "162.158.0.0/15"]
+				  }
+				}
+			  }
+			]
+		  }`},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*",
+				"Condition": {
+				  "Forbidden": true
+				}
+			  }
+			]
+		  }
+		  `},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "s3:GetObject",
+				"Resource": "arn:aws:s3:::logs.countableset.com/*"
+			  }
+			]
+		  }
+		  `},
+		{`{
+			"Version": "2012-10-17",
+			"Statement": [
+			  {
+				"Sid": "PublicReadGetObject",
+				"Effect": "Allow",
+				"Principal": "*",
+				"Action": "route53:Get",
+				"Resource": "arn:aws:*"
+			  }
+			]
+		  }
+		  `},
+	}
+	for _, test := range tests {
+		resp := s3.GetBucketPolicyOutput{Policy: &test.input}
+		policy := parsePolicyResponse(&resp)
+
+		_, result := findBlock(policy)
+		if result != nil {
+			t.Errorf("Expected type of nil")
+		}
 	}
 }
